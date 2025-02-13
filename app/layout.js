258337e4a19eb25665d33dad4b6720a3864564d0
eonsyn <eonsync0@gmail.com>
2025-02-13 23:19:18 +0530

    update the google tag

--- app/layout.js
+++ app/layout.js
@@ -8,10 +8,26 @@ import { SearchProvider } from "@/context/SearchContext";
 import "../styles/globals.css";
 import Authentication from "@/components/authentication/Authentication";
 import Footer from "@/components/basicComponent/Footer";
-
+import Script from "next/script";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || []; 
+              w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'}); 
+              var f = d.getElementsByTagName(s)[0], 
+                  j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; 
+              j.async = true; 
+              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; 
+              f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-5VJB87LR');
+          `}
+        </Script>
+      </head>
       <body className="custom-body-class font-poppins">
         <SearchProvider>
           <SessionProvider>
